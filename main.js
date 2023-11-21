ce32752a8f8df2700a5c6087b3b0e432df2e1575
Joeyiu <147527603+Joeyiu@users.noreply.github.com>
2023-11-21 09:07:24 -0700

    inclass javascript tutorial

---
+++ main.js
@@ -0,0 +1,14 @@
+/* comment 
+multiline */
+
+//alert('Hello, World');
+
+const FIRSTNAME = "Joey"
+let lastName = "Yiu" // E56
+// var lastName = "Yiu"; // E55
+console.log(FIRSTNAME, lastName);
+let age = 21;
+console.log(typeof age, age);
+console.log(FIRSTNAME, lastName);
+age = "twenty"
+age;
