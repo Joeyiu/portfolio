fe18bcdac47ff70d926cdfbb4b35463ae63bcfb2
Joeyiu <147527603+Joeyiu@users.noreply.github.com>
2023-11-21 10:35:34 -0700

    more tutorial update

--- main.js
+++ main.js
@@ -10,5 +10,19 @@ console.log(FIRSTNAME, lastName);
 let age = 21;
 console.log(typeof age, age);
 console.log(FIRSTNAME, lastName);
-age = "twenty"
-age;
+age = "twenty";
+age = 21.5;
+age = true;
+// console.log(typeof age, age);
+// console.log(FIRSTNAME, lastName);
+
+const fullName = (first, last) => {
+    // console.log(first, last);
+    console.log(first, last);
+    // console.log(first + "'s favourite drink is" + drink);
+    console.log(`${first}'s favourite drink is ${drink}`);
+}
+
+fullName("Joey", "Yiu");
+fullName("Carmen", "Lau");
+fullName("Kayla", "Bensing");
