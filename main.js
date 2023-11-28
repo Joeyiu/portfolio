cca914589fc46de37058f779e3012d83636c092c
Joeyiu <147527603+Joeyiu@users.noreply.github.com>
2023-11-28 08:40:49 -0700

    added footer date

--- main.js
+++ main.js
@@ -16,13 +16,48 @@ age = true;
 // console.log(typeof age, age);
 // console.log(FIRSTNAME, lastName);
 
+let drink = "cola";
+//global defining what "drink" is
+
 const fullName = (first, last) => {
     // console.log(first, last);
-    console.log(first, last);
-    // console.log(first + "'s favourite drink is" + drink);
+   // console.log(first + "'s favourite drink is " + drink);
     console.log(`${first}'s favourite drink is ${drink}`);
+    let veggie = "cucumber"; // local scope
+    console.log(veggie);
 }
+
+nameList = ["Joey Yiu", "Carmen Lau", "Kayla Bensing"];
+// square brackets = Array
+nameList.push("Tiana Lewis");
+nameList.push("Jessica Yang");
+console.log(nameList);
+//nameList.pop(); - to get rid of a name
+
+console.log(nameList[1]);
 
 fullName("Joey", "Yiu");
 fullName("Carmen", "Lau");
 fullName("Kayla", "Bensing");
+
+//i=counter ++=+1 --=-1
+for (let i = 0; i < nameList.length; i++) {
+    fullName(nameList[i]);
+}
+
+// while loop
+let j = 0;
+while (j<nameList.length) {
+    fullName(nameList[j]);
+    j++;
+}
+// -----------------------------------------------------------------------------------------------
+
+
+
+
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").innerHTML = year;
+console.log(d, year);
